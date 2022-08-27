9960f08405bd4a603e4fc3bb6cf82b120db377c3
adisk <adiskd22gm@gmail.com>
2022-08-27 19:06:41 +0600

    feat: completed

--- src/components/Banner/Banner.js
+++ src/components/Banner/Banner.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import style from "./Banner.module.css";
+import arrow from "./images/arrow.svg";
+import burger from "./images/burger.png";
 
 const Banner = () => {
     return (
-        <div>
-            
+        <div className={style.move}>
+            <div>
+                <h1 className={style.mainText}>
+                    Доставка вкусной еды <br/>
+                    до 30 минут + напиток <br/>
+                    в подарок!
+                </h1>
+                <div className={style.lastName}>
+                    Доставим заказ вовремя и можете рассчитывать, что <br/>
+                    еда будет доставлен всегда горячим и ароматным.
+                </div>
+                <button className={style.button}>Перейти в меню<img src={arrow}/></button>
+                <img src={burger} alt="Burger" className={style.burger}/>
+            </div>
         </div>
     );
 };
